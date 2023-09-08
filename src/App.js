f59e1c9e47c68afe0e4f997d3a9a5f228768746d
Paul Broccardi <paulbroccardi@gmail.com>
2023-09-08 11:47:53 -0400

    PrototypeUpdate Installed Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="App" class="min-h-screen bg-blue-100">
+      <div>
+        <span>
+          <span class="">Hi, my name is </span>
+          <span class="text-blue-500 font-mono text-xl text-center">Paul Broccardi</span>
+        </span>
+        <span>
+
+        </span>
+        
+        <h2 class=""></h2>
+      </div>
+      <header class="text-lg text-center text-blue-500">
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Ploot-O"
           target="_blank"
           rel="noopener noreferrer"
+          class="p-5 font-light font-mono"
         >
-          Learn React
+          LinkedIn
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/Ploot-O"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="p-5 font-light font-mono"
+        >
+          Github
         </a>
       </header>
     </div>
