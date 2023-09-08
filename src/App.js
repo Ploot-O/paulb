ad2be603b405182bff18604d36c117e3ad229256
Paul Broccardi <paulbroccardi@gmail.com>
2023-09-08 12:01:01 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App" class="min-h-screen bg-blue-100">
-      <div>
+      <div class="text-center">
         <span>
-          <span class="">Hi, my name is </span>
-          <span class="text-blue-500 font-mono text-xl text-center">Paul Broccardi</span>
-        </span>
-        <span>
-
+          <span class="text-blue-500 font-thin text-xl">Hi, my name is </span>
+          <span class="text-blue-500 font-mono text-xl">Paul Broccardi</span>
         </span>
         
-        <h2 class=""></h2>
+        <h2 class="text-blue-500 font-thin text-lg">
+          Networking Associate and Astrophotographer
+        </h2>
       </div>
       <header class="text-lg text-center text-blue-500">
         <a
           className="App-link"
-          href="https://github.com/Ploot-O"
+          href="https://linkedin.com/in/paulbroccardi"
           target="_blank"
           rel="noopener noreferrer"
           class="p-5 font-light font-mono"
@@ -33,6 +32,15 @@ function App() {
           class="p-5 font-light font-mono"
         >
           Github
+        </a>
+        <a
+          className="App-link"
+          href="https://instagram.com/paulb_astro"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="p-5 font-light font-mono"
+        >
+          Instagram
         </a>
       </header>
     </div>
